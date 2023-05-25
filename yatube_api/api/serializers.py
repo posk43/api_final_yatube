764dc814dfeeef99afcb5547782d1f33a54c3b11
Petr Lidzhiev <petyaposk@gmail.com>
2023-05-25 23:17:41 +0300

    end?

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,74 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
+from rest_framework.validators import UniqueTogetherValidator
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    """
+    Сериализатор для модели Post.
+    Включает автора поста по имени пользователя (username).
+    """
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Comment.
+    Включает автора комментария по имени пользователя (username).
+    """
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True, slug_field="username"
     )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Group.
+    Включает все поля модели Group.
+    """
+    class Meta:
+        model = Group
+        fields = ("__all__")
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Follow.
+    Включает пользователя и пользователя, на которого подписываются.
+    Проверяет, что пользователь не может подписываться на самого себя.
+    Применяет валидатор, чтобы проверить, что пользователь не подписан уже на этого пользователя.
+    """
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field="username",
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field="username"
+    )
+
+    def validate_following(self, value):
+        if value == self.context["request"].user:
+            raise serializers.ValidationError("Подписка на себя не возможна")
+        return value
+
+    class Meta:
+        model = Follow
+        fields = "__all__"
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=["user", "following"],
+                message="Вы уже подписаны на этого пользователя",
+            )
+        ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,102 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from posts.models import Group, Post
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Представление для работы с постами.
+    Разрешает просмотр, создание, обновление и удаление постов.
+    Авторизация требуется только для создания, обновления и удаления.
+    Разрешение на запись доступно только автору поста.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    )
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        """
+        Выполняется при создании нового поста.
+        Устанавливает автора поста в текущего пользователя.
+        """
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Представление для работы с комментариями.
+    Разрешает просмотр, создание, обновление и удаление комментариев.
+    Авторизация требуется только для создания, обновления и удаления.
+    Разрешение на запись доступно только автору комментария.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    )
+
+    def get_queryset(self):
+        """
+        Получает запрос комментариев для определенного поста.
+        Использует идентификатор поста из URL-параметра.
+        """
+        post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        """
+        Выполняется при создании нового комментария.
+        Устанавливает автора комментария в текущего
+        пользователя и связывает его с соответствующим постом.
+        """
+        post = get_object_or_404(Post, id=self.kwargs["post_id"])
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Представление только для чтения групп.
+    Разрешает только просмотр групп.
+    Авторизация требуется только для просмотра.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    )
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    """
+    Представление для работы с подписками .
+    Разрешает просмотр, создание, обновление и удаление подписок.
+    Требуется аутентификация пользователя.
+    Поддерживает фильтрацию по имени пользователей, на которых подписан пользователь.
+    """
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("following__username",)
+
+    def get_queryset(self):
+        """Получает запрос подписок для текущего пользователя."""
+        user = self.request.user
+        new_queryset = user.followers.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        """
+        Выполняется при создании новой подписки.
+        Устанавливает пользователя в текущего пользователя.
+        """
+        serializer.save(user=self.request.user)
+
